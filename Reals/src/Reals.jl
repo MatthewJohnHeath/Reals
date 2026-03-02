246c9380e39eaf5bd4fd05c8a237d772c12afa36
MatthewJohnHeath <matthew.john.heath@gmail.com>
2026-03-02 12:29:44 +0000

    WIP regularizing average with zero

--- Reals/src/Reals.jl
+++ Reals/src/Reals.jl
@@ -1,23 +1,27 @@
 module Reals
 import Lazy
 
-abstract type SignedHalfBit end
+abstract type SmallHalfInteger end
+abstract type SignedHalfBit <: SmallHalfInteger end
 
 abstract type SignedBit <: SignedHalfBit end
+
+struct ThreeHalves <: SmallHalfInteger end
 struct One <: SignedBit end
+struct Half <: SignedHalfBit end
 struct Zero <: SignedBit end
+struct NegHalf <: SignedHalfBit end
 struct NegOne <: SignedBit end
+struct NegThreeHalves <: SmallHalfInteger end
 
 function concat_all(xs::Lazy.List)
     Lazy.@lazy Lazy.isempty(xs) ? List() : 
     Lazy.first(xs) : concat_all(Lazy.tail(xs))
 end
 
-
 Base.:-(::One) = NegOne()
 Base.:-(::Zero) = Zero()
 Base.:-(::NegOne) = One()
-
 """
     Base.:-(x::Lazy.LazyList)
 
@@ -25,12 +29,9 @@ TBW
 """
 Base.:-(x::Lazy.LazyList) =  Lazy.map(-, x)
 
-struct Half <: SignedHalfBit end
-struct NegHalf <: SignedHalfBit end
 
 Base.:-(::Half) = NegHalf()
 Base.:-(::NegHalf) = Half()
-
 
 bit_average(first::SignedBit, second::SignedBit) = bit_average(second, first) # commutative
 bit_average(::T, ::T) where T<:SignedBit = T()
@@ -44,6 +45,7 @@ function average_half_bits(xs::Lazy.List, ys::Lazy.List)
 end
 
 Base.:+(::SignedBit, ::SignedHalfBit) = error("Out of range on signed-bit addition")
+Base.:+(::Zero, h::Zero) = h
 Base.:+(::Zero, h::SignedHalfBit) = h
 Base.:+(b::SignedBit, ::Zero) = b
 Base.:+(::One, ::NegOne) = Zero()
@@ -65,7 +67,6 @@ bit_and_carry(::Half, ::One) = (One(), NegOne())
 bit_and_carry(::NegHalf, ::SignedBit) = (Zero(), NegOne())
 bit_and_carry(::NegHalf, ::NegOne) = (NegOne(), One())
 
-
 function dehalf_bits(xs::Lazy.List, carry::SignedBit = Zero())
     Lazy.isempty(xs) && return Lazy.list()  
     target = carry + Lazy.first(xs)
@@ -73,13 +74,28 @@ function dehalf_bits(xs::Lazy.List, carry::SignedBit = Zero())
     Lazy.isempty(tail) && return as_bit_list(target)
     (next, carry) = bit_and_carry(target, Lazy.first(tail))
     return Lazy.@lazy next : dehalf_bits(tail, carry)
-
 end
 
 average(xs::Lazy.List, ys::Lazy.List) = dehalf_bits(average_half_bits(xs, ys))
+zero_front_equiv(::Zero, x::SignedBit) = (Zero(), x)
+zero_front_equiv(::One, ::NegOne) = (Zero(), One())
+unsafe_add(xs::Lazy.List, ys::Lazy.List) = Lazy.tail(average(xs, ys))
 
-small_real_zero = Lazy.@lazy Zero():small_real_zero
-small_real_one = Lazy.@lazy One():small_real_one
+Base.:*(::Zero, ::Lazy.List) = Lazy.list()
+Base.:*(::One, xs::Lazy.List) = xs
+Base.:*(::NegOne, xs::Lazy.List) = -xs
 
+function product_summands(xs::Lazy.List, ys::Lazy.List, right_shift = 1)
+    Lazy.isempty(xs) && return Lazy.list()
+    head = Lazy.first(xs)
+    tail = Lazy.tail(xs)
+    unshifted_summand = head * ys
+    summand = Lazy.repeat(Zero(), right_shift) : unshifted_summand
+    return Lazy.@lazy summand : product_summands(tail, ys, right_shift + 1)
+end
+
+zeroes = Lazy.@lazy Zero() : zeroes
+one = Lazy.@lazy One() : one
+long_half = Zero():one
 
 end # module Reals
